be7c7658882bce5ad7888e0eb277c0e4963ef1ab
Cotten-Lumb03 <cottenlumb@ufl.edu>
2023-07-19 09:41:36 -0400

    added decode() to Jorge_encoder wip

--- Jorge_encoder.py
+++ Jorge_encoder.py
@@ -3,9 +3,9 @@ def menu():
     print("Menu\n-------------\n1. Encode\n2. Decode\n3, Quit")
 
 
-def encode(password):
+def encode(input_password):
     output = ""
-    for num in str(password):  # turn password into string to iterate through every character       
+    for num in str(input_password):  # turn password into string to iterate through every character
         if num == "0":  # detects 0, as int() doesn't
             num = str(3)
         else:   # any digit besides 0 
@@ -18,6 +18,18 @@ def encode(password):
 
 
 def decode(password):
+    decoded_password = ''
+    for num in str(password):
+        if num == '0':
+            num = str(7)
+        elif num == '1':
+            num = str(8)
+        elif num == '2':
+            num = str(9)
+        else:
+            num = str(int(num) - 3)
+        decoded_password += num
+    print(f"Your decoded password is {password}, and your original password is {decoded_password}")
     pass
 
 
@@ -26,10 +38,10 @@ while True:
     option = int(input("Please enter an option:"))
 
     if option == 1:
-        password = input("Please enter your password to encode:")
-        print(encode(password))
+        password_input = input("Please enter your password to encode:")
+        print(encode(password_input))
     elif option == 2:
-        # decode()
+        decode(password)
         pass
     elif option == 3:
         break
